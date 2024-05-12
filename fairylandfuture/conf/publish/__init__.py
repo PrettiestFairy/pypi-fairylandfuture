5b47130b53a15611fcdcadc272e0721b3cef4c96
Lionel Johnson's PC for work <austinfairyland@yeah.net>
2024-05-12 11:28:02 +0800

    :bug: Remove unused __init__.py in publish directory
    This commit involves the deletion of an unnecessary __init__.py file in the publish directory within the fairylandfuture/conf. This action helps to prevent clutter and confusion moving forward.

--- fairylandfuture/conf/publish/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf8
-"""
-@software: PyCharm
-@author: Lionel Johnson
-@contact: https://fairy.host
-@organization: https://github.com/FairylandFuture
-@since: 2024-05-11 18:55:09 UTC+8
-"""
