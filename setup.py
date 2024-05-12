465d3f8162052ad4f0053a3c6c81b5404f23ec03
Lionel Johnson's PC for work <austinfairyland@yeah.net>
2024-05-12 13:50:17 +0800

    :package: Update package keywords and remove print statement
    Expanded the list of keywords in the setup.py file to include "fairylandfuture", "PyFairylandFuture", and "FairylandFuture". Also, the print statement that was printing the package version when setup.py was run as a script has been removed.

--- setup.py
+++ setup.py
@@ -157,7 +157,17 @@ class PackageInfo(object):
 
     @property
     def keywords(self):
-        return ["fairyland", "Fairyland", "pyfairyland", "PyFairyland", "fairy", "Fairy"]
+        return [
+            "fairyland",
+            "Fairyland",
+            "pyfairyland",
+            "PyFairyland",
+            "fairy",
+            "Fairy",
+            "fairylandfuture",
+            "PyFairylandFuture",
+            "FairylandFuture",
+        ]
 
     @property
     def include_package_data(self):
@@ -280,6 +290,3 @@ setuptools.setup(
     install_requires=package.install_requires,
     cmdclass=package.cmdclass,
 )
-
-if __name__ == "__main__":
-    print(package.version)
