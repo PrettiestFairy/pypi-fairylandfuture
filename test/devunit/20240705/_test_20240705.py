678905260388075b05105be6907326a4311e5ede
Lionel Johnson(Work) <fariylandfuture@outlook.com>
2024-07-05 13:56:10 +0800

    :sparkles: Add PostgreSQL database connector and related refactoring
    Introduces a new PostgreSQLConnector class in the databases module. It ensures a custom connection and cursor tracking with respective tests. Adjustments have also been made to error handling in the close method in the MySQL module. Additionally, configurations and dependencies were added for the PostgreSQL capabilities. Lastly, previous tests were moved to the archive directory.

---
+++ test/devunit/20240705/_test_20240705.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+""" 
+@software: PyCharm
+@author: Lionel Johnson
+@contact: https://fairy.host
+@organization: https://github.com/FairylandFuture
+@since: 2024-07-05 12:43:37 UTC+8
+"""
+
+from typing import Dict, Any, List, NamedTuple
+from pathlib import Path
+
+from test.utils.config import TestConfig
+from fairylandfuture.modules.databases.postgresql import PostgreSQLConnector
+
+class SQLData(NamedTuple):
+    feature_id: str
+    feature_name: str
+    sub_feature_id: str
+    sub_feature_name: str
+    is_supported: str
+    is_verified_by: str
+    comments: str
+
+config: Dict[str, Any] = TestConfig(Path(r"C:\Lionel\Project\Github\pypi-fairylandfuture\conf\dev\config.yaml")).config.get("postgresql")
+print(repr(config))
+
+pg = PostgreSQLConnector(
+    host=config.get("host"),
+    port=config.get("port"),
+    user=config.get("user"),
+    password=config.get("password"),
+    database=config.get("database"),
+)
+
+pg.cursor.execute("select * from information_schema.sql_features limit 10 offset 0;")
+data: List[SQLData] = pg.cursor.fetchall()
+pg.cursor.close()
+for i in data:
+    print(i, i.feature_id)
+pg.reconnect()
+pg.cursor.execute("select * from information_schema.sql_features limit 10 offset 10;")
+data2 = pg.cursor.fetchall()
+pg.cursor.close()
+pg.close()
+
+
+from collections import namedtuple
+
+
+column_name = ["a", "b", "c"]
+row_1_data = [1,2,3]
+row_2_data = [1,2,3]
+
+a = map(lambda x:namedtuple("Data", column_name)(*x), (row_1_data, row_2_data))
+for i in a:
+    print(i, i.a, i.b, i.c)
+
+print(pg.dsn.__repr__())
