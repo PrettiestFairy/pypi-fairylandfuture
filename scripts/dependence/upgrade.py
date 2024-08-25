267b1f4ef04179277534f161efcaa7f2b7679e10
Lionel Johnson(Work) <fariylandfuture@protonmail.com>
2024-08-25 11:57:00 +0800

    :bug: Update requirements generation and add file copy
    Renamed the output file for requirements to `release-win-requirements.txt` and introduced a step to copy it to `requirements.txt`. Also added the root path to the system path before changing the working directory.

--- scripts/dependence/upgrade.py
+++ scripts/dependence/upgrade.py
@@ -29,9 +29,11 @@ def main():
 
     print("Step 2: Generate requirements.")
     # generate_requirements = "pip-compile --upgrade --strip-extras --generate-hashes requirements.in"
-    generate_requirements = "pip-compile --upgrade --strip-extras --generate-hashes --output-file=requirements.txt requirements.in"
+    generate_requirements = "pip-compile --upgrade --strip-extras --generate-hashes --output-file=release-win-requirements.txt requirements.in"
     # The output information is in the error channel
+    copy_requirements = "copy release-win-requirements.txt requirements.txt"
     subprocess.Popen(generate_requirements.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    subprocess.Popen(copy_requirements.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     print("Generate requirements successfully.")
 
     print("Step 3: Sync dependencies.")
@@ -60,5 +62,6 @@ def main():
 
 
 if __name__ == "__main__":
+    sys.path.append(ROOT_PATH)
     os.chdir(ROOT_PATH)
     main()
