ee9ac41aa7692160c78329a4be7ec109525b9267
Lionel Johnson(Work) <fariylandfuture@protonmail.com>
2024-09-24 15:56:57 +0800

    :fire: Remove unused validation toolkit and type hint files
    Deleted the initialization and validator type hint files under the fairylandfuture toolkit, along with the mypy configuration file, as they are no longer in use. These removals streamline the project by eliminating deprecated or redundant code.

--- types/fairylandfuture/toolkits/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-"""
-@software: PyCharm
-@author: Lionel Johnson
-@contact: https://fairy.host
-@organization: https://github.com/FairylandFuture
-@datetime: 2024-09-24 15:41:05 UTC+08:00
-"""
--- types/fairylandfuture/toolkits/validation/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-"""
-@software: PyCharm
-@author: Lionel Johnson
-@contact: https://fairy.host
-@organization: https://github.com/FairylandFuture
-@datetime: 2024-09-24 15:41:14 UTC+08:00
-"""
