5aa87c3bd64971c8eb532c1c52ee28cbc1e51f34
Lionel Johnson(work) <fariylandfuture@protonmail.com>
2024-12-10 14:30:41 +0800

    :art: Add test file for 20241030 and archive older tests
    Created a new test file under test/devunit for the date 20241030. Additionally, moved previous test files from 20241015 into an archive directory to organize and maintain older tests separately.

---
+++ test/devunit/20241030/_test.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+"""
+@software: PyCharm
+@author: Lionel Johnson
+@contact: https://fairy.host
+@organization: https://github.com/FairylandFuture
+@datetime: 2024-10-30 14:34:05 UTC+08:00
+"""
+
+import os
+import sys
+
+# os.getcwd() == os.path.dirname(__file__)
+current_path = os.getcwd()
+current_path_2 = os.path.dirname(__file__)
+
+print(current_path)
+print(current_path_2)
